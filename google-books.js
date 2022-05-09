a63a01d43aa41e7485b76c5cdda921d3c1a01a8d
unknown <martin.michellex@gmail.com>
2022-05-09 22:40:44 +1000

    HTML added, scss updated, begin JS work

---
+++ google-books.js
@@ -0,0 +1,11 @@
+// When the submit button is clicked you need the request books from the Google books API using the input value as your query string
+// Create your input and submit button
+// Submit button to have an on-click event to request google books API
+// Input value would execute a function..
+
+// The books that you receive should be rendered in the books grid.
+// Each book in the grid should have an image, author, title and description
+//     ○ New object pulling specific data: image, author, title, description
+
+// The grid should be responsive on different screen sizes
+// You should use async / await for your request code, NOT .then
