a043a090f4aefbbd5160bc4feef5c92ae2ac6f5c
unknown <martin.michellex@gmail.com>
2022-05-13 15:09:19 +1000

    Notes from martyna! fixed DOM. Pending image added

--- dom.js
+++ dom.js
@@ -1,1 +1,91 @@
-// Add the function for the loop: placeholders/arguments
+const renderBooks = (array) => {
+    const myBooks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for (let i = 0; i < myBooks.length; i++) {
+        // let gridCellNum = myBooks[i];
+
+        // console.log(document.getElementById("grid-" + gridCellNum).children);
+
+        const outsideDiv = document.createElement("div");
+
+        // BOOK IMAGE
+        const newElementImage = document.createElement("img");
+        newElementImage.setAttribute("src", array[resultNumber].Image);
+        newElementImage.setAttribute("alt", "na");
+        const parentImage = document.getElementById("results" + resultNumber);
+        parentImage.appendChild(newElementImage);
+
+        //BOOK TITLE
+        const titleElement = document.createElement("h4");
+        const titleTextNode = document.createTextNode(array[i].bookTitle);
+
+        titleElement.appendChild(titleTextNode);
+
+        // const addDivBookTitle = document.getElementById("grid");
+        // outsideDiv.appendChild(titleTextNode);
+
+        // BOOK AUTHOR
+        const authorElement = document.createElement("h5");
+        const authorTextNode = document.createTextNode(array[i].bookAuthors);
+
+        authorElement.appendChild(authorTextNode);
+
+        // const addDivBookAuthor = document.getElementById("grid");
+        // outsideDiv.appendChild(authorTextNode);
+
+        // BOOK DESCRIPTION
+
+        const descriptionElement = document.createElement("p");
+        const descriptionTextNode = document.createTextNode(
+            array[i].bookDescription
+        );
+
+        descriptionElement.appendChild(descriptionTextNode);
+
+        // const addDivBookDescription = document.getElementById("grid");
+        // addDivBookDescription.appendChild(descriptionTextNode);
+
+        // // appending to div
+        outsideDiv.appendChild(titleElement);
+        outsideDiv.appendChild(authorElement);
+        outsideDiv.appendChild(descriptionElement);
+
+        // WHERE WE'RE HOUSING OUR OUTSIDE DIV TO LIVE IN THE HTML/PAGE:
+        document.querySelector(".grid").appendChild(outsideDiv);
+    }
+};
+
+export default renderBooks;
+
+// addDescription.appendChild(descriptionParagraph);
+
+// const addDivBookDescription = document.getElementById("grid");
+// addDivBookDescription.appendChild(descriptionParagraph);
+
+// const findDescription = document.getElementById("grid-" + gridCellNum);
+// findDescription.appendChild(addDescription);
+// const findTitle = document.getElementById("grid-" + gridCellNum);
+// findTitle.appendChild(addTitle);
+
+// // Authors
+// const addAuthor = document.createElement("h5");
+// const authorNames = document.createTextNode(array[i].bookAuthors);
+
+// addAuthor.appendChild(authorNames);
+
+// const addDivBookAuthor = document.getElementById("grid");
+// addDivBookAuthor.appendChild(authorNames);
+
+// // const findAuthor = document.getElementById("grid-" + gridCellNum);
+// // findAuthor.appendChild(addAuthor);
+
+// // Description
+
+// const addDescription = document.createElement("p");
+// const descriptionParagraph = document.createTextNode(
+//     array[i].bookDescription
+// );
+
+// creating an outside div (box), for the other elements (i.e. h4,h5, p elements). then creating text nodes => likning textnodes with HTML elements => grabbing all HTML elements containing text and APPENDING (Attaching them to the box - the outside DIV)
+// the OUTSIDE DIV (containing children) is being displayed in the grid
+// the DIV has been created in the DOM ==> needs to point it somwehere on the page
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+import renderBooks from "./dom.js";
+
 // FUNCTION: concatenating the search term with the API url
 const urlBase = "https://www.googleapis.com/books/v1/volumes?q=";
-getSearchUrl = (searchTerm) => {
+const getSearchUrl = (searchTerm) => {
     return urlBase + searchTerm;
 };
 
 // FUNCTION: Getting data from the API
 const getGoogleApi = async (searchQuery) => {
+    // clearResults();
+
     const requestPromise = fetch(getSearchUrl(searchQuery));
     const response = await requestPromise;
     const searchData = await response.json();
+
+    console.log(searchData);
 
     const bookData = searchData.items.map((x) => {
         let output = {};
@@ -54,94 +60,24 @@ const getGoogleApi = async (searchQuery) => {
 
         return output;
     });
-
-    // FUNCTION: Render bookData on the front end
-    // I need something where this bookData information needs to be either stored somewhere to execute a function later to render the data on the front end.
-    // console.log(bookData[0].bookImage);
-
-    // DOM FUNCTION HERE - IMPORT
-
-    const myBooks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    for (let i = 0; i < myBooks.length; i++) {
-        let gridCellNum = myBooks[i];
-
-        //Title
-        const addTitle = document.createElement("h4");
-        const titleName = document.createTextNode(bookData[i].bookTitle);
-
-        addTitle.appendChild(titleName);
-
-        const findTitle = document.getElementById("grid-" + gridCellNum);
-        findTitle.appendChild(addTitle);
-
-        // Authors
-        const addAuthor = document.createElement("h5");
-        const authorNames = document.createTextNode(bookData[i].bookAuthors);
-
-        addAuthor.appendChild(authorNames);
-
-        const findAuthor = document.getElementById("grid-" + gridCellNum);
-        findAuthor.appendChild(addAuthor);
-
-        // Description
-
-        const addDescription = document.createElement("p"); //creating a paragraph to house my description
-        const descriptionParagraph = document.createTextNode(
-            bookData[i].bookDescription
-        ); // adding the bookDescription data from the API to be created as text.
-
-        addDescription.appendChild(descriptionParagraph); //add the results of descriptionParagraph (api results) to the addDescription paragraph element
-
-        const findDescription = document.getElementById("grid-" + gridCellNum);
-        findDescription.appendChild(addDescription); // shove the element you created in the placeholder of your actual HTML doc
-    }
-
-    // const newElement2 = document.createElement("h5");
-    // const textNode2 = document.createTextNode(bookData[0].bookAuthors);
-    // newElement2.appendChild(textNode2);
-    // const findParent2 = document.getElementById("grid-1");
-    // findParent.appendChild(newElement2);
-
-    // const newElement3 = document.createElement("p");
-    // const textNode3 = document.createTextNode(bookData[0].bookDescription);
-    // newElement2.appendChild(textNode3);
-    // const findParent3 = document.getElementById("grid-1");
-    // findParent.appendChild(newElement3);
-
-    // const newElementAuthors = document.createElement("h5");
-    // const textNodeAuthors = document.createTextNode(bookData[0].bookAuthors);
-    // newElement.appendChild(textNodeAuthors);
-
-    // const findParent = document.getElementById("grid-1");
-    // findParent.appendChild(newElementAuthors);
-
-    // const myBooks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    // for (let i = 0; i < myBooks.length; i++) {
-    //     let gridCellNum = myBooks[i];
-    //     document.getElementById("title" + gridCellNum).innerText =
-    //         bookData[gridCellNum].bookTitle;
-
-    //     document.getElementById("author" + gridCellNum).innerText =
-    //         bookData[gridCellNum].bookAuthors;
-
-    //     document.getElementById("description" + gridCellNum).innerText =
-    //         bookData[gridCellNum].bookDescription;
-
-    //     // document.getElementById("bookCover" + gridCellNum).innerText =
-    //     //     bookData[gridCellNum].bookImage;
-
-    //     document.getElementById("bookCover" + gridCellNum).src =
-    //         bookData[gridCellNum].bookImage.thumbnail; //nested objects
-
-    //     // console.log(bookData[divNum].bookImage.thumbnail); ==> SRC
-    // }
+    renderBooks(bookData); //calling the function from modules
 };
 
 // FUNCTION: Listener event on search button
 const searchInput = document.getElementById("search_input");
 const searchButton = document.getElementById("search-btn");
+
 searchButton.addEventListener("click", () => {
+    const removeAllChildren = (parent) => {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    };
+    removeAllChildren(document.getElementById("grid"));
+
     getGoogleApi(searchInput.value);
 });
+
+// parent is the DIV holding children
+// firstChild is a property from JS DOM - will give you the first element nested inside it (i.e DIV of the very first book cell)
+// while the value is TRUTHY --> the loop is happening until NO children is left and the DIV becomes empty
